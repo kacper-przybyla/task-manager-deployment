dd5d4cbe1f0bc1b78b0e9b1a1ecb283d01afd1c3
Kacper <kacper.przybyla.praca@gmail.com>
2026-05-05 19:17:26 +0200

    fix: add defaults to variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,23 @@
 variable "aws_region" {
     description = "Region of AWS"
     type = string
+    default = "eu-central-1"
 }
 
 variable "environment" {
     description = "Deployment environment"
     type = string
+    default = "dev"
 }
 
 variable "project_name" {
     description = "Name of the project"
     type = string
+    default = "task-manager"
 }
 
 variable "instance_type" {
     description = "EC2 instance type"
     type = string
+    default = "t3.micro"
 }
